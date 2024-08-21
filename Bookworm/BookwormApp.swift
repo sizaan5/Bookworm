1845ac9d5301201d251d3c6165e4d58a8a6f2ef6
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2024-08-21 17:04:57 +0500

    initial commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Izaan Saleem on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
