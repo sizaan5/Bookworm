226cf4ef4fed5d3044875cfc42be195ee69251ca
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2024-08-22 12:49:48 +0500

    Day 53/100 - Creating project, custom component with @Binding and @Bindable and multiline with TextField and TextEditor

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -7,15 +7,48 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button(title) {
+            isOn.toggle()
         }
         .padding()
+        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var rememberMe = false
+    @AppStorage("notes") private var notes = ""
+    @AppStorage("notes1") private var notes1 = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                PushButton(title: "Remember Me", isOn: $rememberMe)
+                Text(rememberMe ? "On" : "Off")
+                
+                TextEditor(text: $notes)
+                                .navigationTitle("Notes")
+                                .padding()
+                                .border(.black, width: 1)
+                TextField("Enter your text", text: $notes1, axis: .vertical)
+                                .textFieldStyle(.roundedBorder)
+                                .navigationTitle("Notes")
+                                .padding()
+            }
+            .padding()
+        }
     }
 }
 
